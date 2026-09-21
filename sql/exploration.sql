99c840a0795b489ad18cf1344131871c1926a244
Renan Croffi <renancroffi@gmail.com>
2026-09-21 18:13:42 -0300

    fix comentarios e nomeacao de variavel

--- sql/exploration.sql
+++ sql/exploration.sql
@@ -1,4 +1,4 @@
--- existe sazonalidade visivel por mes? t
+-- 1. Existe sazonalidade visível por mês?
 SELECT
     EXTRACT(MONTH FROM dia) AS month,
     TRUNC(AVG(temp_max)::numeric, 2) AS avg_temperature_max,
@@ -6,23 +6,30 @@ SELECT
     TRUNC(AVG(precipitacao)::numeric, 2) AS avg_precipitation
 FROM weather_daily
 GROUP BY month
-ORDER BY month; 
+ORDER BY month;
 
--- podemos ver que existe sazonalidade visivel por mes, com junho e julho mais frios e secos e dezembro a março mais quente e chuvoso.
+-- Conclusão: existe sazonalidade visível por mês, com junho e julho mais frios e secos,
+-- e dezembro a março mais quente e chuvoso.
 
--- Precipitação correlaciona com temperatura?
+
+-- 2. Precipitação correlaciona com temperatura?
 SELECT
     TRUNC(CORR(temp_max, precipitacao)::numeric, 2) AS correlation_temp_max_precipitation,
     TRUNC(CORR(temp_min, precipitacao)::numeric, 2) AS correlation_temp_min_precipitation
 FROM weather_daily;
 
--- existe correlação positiva entre a temperatura minima e precipitação, o que parece ser contra intuitivo, mas pode ser explicado pelo fato de que em dias mais quentes, a evaporação é maior, formando uma camada de nuvens que funcionam como um cobertor. 
+-- Conclusão: existe correlação positiva entre a temperatura mínima e a precipitação, o que
+-- parece contraintuitivo, mas pode ser explicado pelo fato de que em dias nublados as nuvens
+-- funcionam como um cobertor, segurando parte do calor que sairia pra atmosfera e fazendo
+-- com que a temperatura não caia tanto.
 
---Qual variável tem maior desvio dia a dia?
+
+-- 3. Qual variável tem maior desvio dia a dia?
 SELECT
-    TRUNC(STDDEV(temp_max)::numeric, 2)/TRUNC(AVG(temp_max)::numeric, 2) AS stddev_temp_max,
-    TRUNC(STDDEV(temp_min)::numeric, 2)/TRUNC(AVG(temp_min)::numeric, 2) AS stddev_temp_min,
-    TRUNC(STDDEV(precipitacao)::numeric, 2)/TRUNC(AVG(precipitacao)::numeric, 2) AS stddev_precipitation
-FROM weather_daily; 
+    TRUNC((STDDEV(temp_max)/AVG(temp_max))::numeric, 2) AS cv_temp_max,
+    TRUNC((STDDEV(temp_min)/AVG(temp_min))::numeric, 2) AS cv_temp_min,
+    TRUNC((STDDEV(precipitacao)/AVG(precipitacao))::numeric, 2) AS cv_precipitation
+FROM weather_daily;
 
--- podemos ver que a variável com maior desvio dia a dia é a precipitação pois a região tem epócas de chuvas e secas muito bem definidas.
+-- Conclusão: a variável com maior desvio relativo dia a dia é a precipitação, pois a região
+-- tem épocas de chuva e seca muito bem definidas.
